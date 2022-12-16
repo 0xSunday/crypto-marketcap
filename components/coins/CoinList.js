40ccc9d1c0ae8572a89f292cf57c4431c6950180
0xSunil <sunday7637@gmail.com>
2022-12-16 20:26:26 +0530

    styles added

--- components/coins/CoinList.js
+++ components/coins/CoinList.js
@@ -5,10 +5,10 @@ const CoinList = (props) => {
   const { items } = props;
 
   return (
-    <ul className="flex flex-col w-full p-5 sm:p-10  ">
+    <ul className="flex flex-col w-full p-5 sm:p-10 ">
       <div className="flex flex-row sm:justify-between justify-between py-2 ">
-        <p className=" w-full ml-14">Name</p>
-        <p className=" w-full text-center sm:text-start sm:-ml-8 ">price</p>
+        <p className=" w-full ml-9 sm:ml-14">Name</p>
+        <p className=" w-full ml-7 text-center sm:text-start sm:-ml-8  ">price</p>
         <p className="hidden md:block w-full p">24h %</p>
         <p className="hidden sm:block w-full ">maketcap</p>
       </div>
